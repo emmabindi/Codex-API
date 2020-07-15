f1c1cecea38346bd2e1b1e13655bb6b45150eafb
Emma Bindi <emma.bindi@outlook.com>
2020-07-15 15:50:04 +1000

    Removed code to auth using username and reverted back to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
-
-  def self.from_token_request request
-    username = request.params["auth"] && request.params["auth"]["username"]
-    self.find_by username: username
-    end
 end
