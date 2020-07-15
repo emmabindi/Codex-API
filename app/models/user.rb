7befdee5fe51139142cf14e24bfffb477151501d
Emma Bindi <emma.bindi@outlook.com>
2020-07-16 09:04:37 +1000

    Added RSPEC tests for user to validate uniqueness and presence of email and username as well as secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {build(:user)}
+
+  context 'validations' do
+    it 'has a valid factory' do 
+      expect(build(:user)).to be_valid
+    end
+
+    it { expect(subject).to validate_presence_of(:email) }
+    it { expect(subject).to validate_presence_of(:username) }
+    it { expect(subject).to validate_uniqueness_of(:email) }
+    it { expect(subject).to have_secure_password }
+  end
 end
