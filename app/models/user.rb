16a52dd34282bd6f01a58ee5b7bb90b2e8626392
Emma Bindi <emma.bindi@outlook.com>
2020-07-15 15:47:19 +1000

    Setup routes for user and created user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true
   validates :email, presence: true
+
+  def self.from_token_request request
+    username = request.params["auth"] && request.params["auth"]["username"]
+    self.find_by username: username
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post "/login", to: "user_token#create"
   get "/status", to: "status#index"
   get "/status/user", to: "status#user"
+  post "/sign-up", to: "users#create"
 end
