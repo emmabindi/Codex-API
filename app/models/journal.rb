dd85fd97b5906eee22a73c3c9998651c39aa5005
Emma Bindi <emma.bindi@outlook.com>
2020-07-15 13:53:35 +1000

    Rspec tests created for journal body and validations added

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,4 +1,9 @@
 class Journal < ApplicationRecord
   validates :title, presence: true
   validates_length_of :title, :within => 3..40, :too_long => "Please enter a shorter title", :too_short => "Please enter a longer title"
+
+  validates :body, presence: true
+  validates_length_of :body, :within => 5..15000
+
+  # , :too_long => "Please enter a shorter journal of less than 15000 characters", :too_short => "Please enter a longer journal entry"
 end
--- spec/models/journal_spec.rb
+++ spec/models/journal_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Journal, type: :model do
-  subject { described_class.new(
-    title: 'All About React Hooks',
-    body: 'Today I learned about hooks in React in particular useState.'
-  )}
+  subject {build(:journal)}
 
   context 'validations' do
     it 'is valid with valid attributes' do 
@@ -25,5 +22,14 @@ RSpec.describe Journal, type: :model do
       subject.title = '12345678901234567890123456789012345678901234567890'
       expect(subject).to_not be_valid
     end
+    
+    it 'is not valid without a body' do
+      subject.body = nil
+      expect(subject).to_not be_valid
+    end
+
+    it { should validate_length_of(:body).is_at_least(5) }
+    it { should validate_length_of(:body).is_at_most(15000) }
+
   end
 end
