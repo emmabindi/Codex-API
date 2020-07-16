fc8fbd4b8beb9176390e2c6fac4a5fed924ca98d
Emma Bindi <emma.bindi@outlook.com>
2020-07-16 20:21:14 +1000

    Category model validations and associations RSPEC tests added

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {described_class.new(
+    name: "Back End",
+    user_id: 1
+  )}
+
+  context 'validations' do
+    it {expect(subject).to validate_presence_of(:name)}
+
+    it do
+      should validate_length_of(:name).
+      is_at_least(2).
+      with_short_message('Please enter a longer category name')
+    end
+
+    it do
+      should validate_length_of(:name).
+      is_at_most(40).
+      with_long_message('Please enter a shorter category name of less than 40 characters')
+    end
+  end
+
+  context 'associations' do
+    it { expect(subject).to belong_to(:user)}
+  end
 end
