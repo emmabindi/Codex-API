b53eda1f10ff02a3f9543560b829d9f231295dca
Emma Bindi <emma.bindi@outlook.com>
2020-07-17 09:10:13 +1000

    Added rspec test for requests bookmarks for a user - passing

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
+  it 'responds successfully' do
+    get '/bookmarks', headers: authenticated_header
+  
+    expect(response).to have_http_status(:success)
+  end
 end
---
+++ spec/support/authentication_helpers.rb
@@ -0,0 +1,13 @@
+module AuthenticationHelpers
+  def authenticated_header
+    user = create(:user)
+
+    token = Knock::AuthToken.new(payload: {sub: user.id}).token
+
+    { 'Authorization': "Bearer #{token}" }
+  end
+end
+
+RSpec.configure do |config|
+  config.include AuthenticationHelpers, type: :request
+end
