ca144ab5625a29f6d866cd5505bf7f170c1a13ec
Emma Bindi <emma.bindi@outlook.com>
2020-07-24 09:38:20 +1000

    Added daily activity data search and weekly - each model and also a total

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -40,6 +40,32 @@ class AnalyticsController < ApplicationController
     total_entries_by_language = ar.inject{|memo, el| memo.merge( el ){|k, old_v, new_v| old_v + new_v}}
 
 
+    # Activity by Day 
+
+    bookmark_daily_count = current_user.bookmarks.where(:created_at => (Time.now.midnight - 1.day)..Time.now.midnight).count
+    goals_daily_count = current_user.goals.where(:created_at => (Time.now.midnight - 1.day)..Time.now.midnight).count
+    journals_daily_count = current_user.journals.where(:created_at => (Time.now.midnight - 1.day)..Time.now.midnight).count
+    daily_activity = bookmark_daily_count + goals_daily_count + journals_daily_count
+
+    # Activity by week 
+
+    bookmark_week_count = current_user.bookmarks.where(:created_at => (Time.now.midnight - 7.days)..Time.now.midnight).count
+    goals_week_count = current_user.goals.where(:created_at => (Time.now.midnight - 7.days)..Time.now.midnight).count
+    journals_week_count = current_user.journals.where(:created_at => (Time.now.midnight - 7.days)..Time.now.midnight).count
+    weekly_activity = bookmark_week_count + goals_week_count + journals_week_count
+
+  #   MyModel.where("created_at < ?", 2.days.ago)
+
+  #   MyTable.where(created_at: Date.new..2.days.ago)
+
+  # SELECT * FROM clients WHERE (clients.created_at BETWEEN '2008-12-21 00:00:00' AND '2008-12-22 00:00:00')
+
+  # Client.where(
+  #     "created_at >= ? AND created_at <= ?", params[:start_date], params[:end_date])
+
+  #   Model.where("created_at IN (?)",
+  # (params[:start_date].to_date)..(params[:end_date].to_date))
+
     render json: { 
       bookmarks_total: bookmarks_total, 
       journals_total: journals_total, 
@@ -55,6 +81,16 @@ class AnalyticsController < ApplicationController
       goals_by_language: goals_by_language, 
       journals_by_language: journals_by_language,
       total_entries_by_language: total_entries_by_language, 
+
+      bookmark_daily_count: bookmark_daily_count,
+      journals_daily_count: journals_daily_count,
+      goals_daily_count: goals_daily_count,
+      daily_activity: daily_activity,
+
+      bookmark_week_count: bookmark_week_count,
+      journals_week_count: journals_week_count,
+      goals_week_count: goals_week_count,
+      weekly_activity: weekly_activity,
     }
   end
 end
