57e15431fdb3af5bae8b7240e19df27487d5f562
Emma Bindi <emma.bindi@outlook.com>
2020-07-25 15:18:34 +1000

    RSPEC tests added for analytics requests

--- spec/requests/analytics_request_spec.rb
+++ spec/requests/analytics_request_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Analytics', type: :request do
+
+  describe "GET /analytics/counts" do
+    it "returns http success" do
+      get "/analytics/counts", headers: authenticated_header
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /analytics/activity" do
+    it "returns http success" do
+      get "/analytics/activity", headers: authenticated_header
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /analytics/categories" do
+    it "returns http success" do
+      get "/analytics/categories", headers: authenticated_header
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /analytics/languages" do
+    it "returns http success" do
+      get "/analytics/languages", headers: authenticated_header
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
