5378a6949fea9f934b44f7fe5bc8f382ae80d7b0
Emma Bindi <emma.bindi@outlook.com>
2020-07-29 07:54:36 +1000

    Timer request RSPEC get passing

--- spec/requests/timer_request_spec.rb
+++ spec/requests/timer_request_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe "Timers", type: :request do
 
   describe "GET #index" do
-    # it "responds successfully to retrieve a users timer total" do
-    #   get "/timer", headers: authenticated_header
-    #   expect(response).to have_http_status(:ok)
-    # end
+    it "responds successfully to retrieve a users timer total" do
+      get "/timer", headers: authenticated_header
+      expect(response).to have_http_status(:ok)
+    end
   end
 
 
