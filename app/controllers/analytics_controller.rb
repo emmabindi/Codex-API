cd8a664a7905e33ab8f7fbdda77ce8c0305fc21f
Emma Bindi <emma.bindi@outlook.com>
2020-08-22 13:06:31 +1000

    Rubocop offences corrected, useless variables and qhwitespace removed

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -7,8 +7,8 @@ class AnalyticsController < ApplicationController
       total_entries_by_date: current_user.fetch_activities_by_date(:bookmarks, :goals, :journals),
       bookmarks: current_user.fetch_activity_by_date(:bookmarks),
       goals: current_user.fetch_activity_by_date(:goals),
-      journals: current_user.fetch_activity_by_date(:journals),
-    } 
+      journals: current_user.fetch_activity_by_date(:journals)
+    }
   end
 
   #   get '/analytics/categories', to: 'analytics#categories'
@@ -27,32 +27,32 @@ class AnalyticsController < ApplicationController
       total_entries_by_language: current_user.fetch_activities_by_language(:bookmarks, :goals, :journals),
       bookmarks_by_language: current_user.fetch_activity_by_language(:bookmarks),
       goals_by_language: current_user.fetch_activity_by_language(:goals),
-      journals_by_language: current_user.fetch_activity_by_language(:journals),
+      journals_by_language: current_user.fetch_activity_by_language(:journals)
     }
   end
 
   def counts
     # Activity by week
     # weekly_activity = bookmark_week_count + goals_week_count + journals_week_count
-    
+
     render json: {
       bookmarks_total: current_user.bookmarks.length,
       journals_total: current_user.journals.length,
       completed_goals: current_user.goals.where(completed: true).length,
       active_goals: current_user.goals.where(completed: false).length,
-      
+
       # Activity by Day
       bookmark_daily_count: current_user.fetch_daily_counts(:bookmarks),
       journals_daily_count: current_user.fetch_daily_counts(:journals),
       goals_daily_count: current_user.fetch_daily_counts(:goals),
-      
+
       # won't work because need to total these before sending as json
       # daily_activity: (bookmark_daily_count + goals_daily_count + journals_daily_count),
 
       # Activity by week
       bookmark_week_count: current_user.fetch_weekly_counts(:bookmarks),
       journals_week_count: current_user.fetch_weekly_counts(:journals),
-      goals_week_count: current_user.fetch_weekly_counts(:goals),
+      goals_week_count: current_user.fetch_weekly_counts(:goals)
       # weekly_activity: weekly_activity
     }
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :journals, dependent: :destroy
   has_many :timers, dependent: :destroy
 
-  # Gets entries by date for bar graphs 
+  # Gets entries by date for bar graphs
   def fetch_activity_by_date(type)
     public_send(type).group_by_day(
       :created_at,
@@ -17,63 +17,53 @@ class User < ApplicationRecord
     ).count
   end
 
-  # Gets a hash containing the counts of the users activity and total of all types 
+  # Gets a hash containing the counts of the users activity and total of all types
   def fetch_activities_by_date(*types)
-  entries_array = []
-    types.each_with_object({}) do | key, hash | 
-      entries_array.push(hash[key] = fetch_activity_by_date(key)) 
+    entries_array = []
+    types.each_with_object({}) do |key, hash|
+      entries_array.push(hash[key] = fetch_activity_by_date(key))
     end
-    total_entries_by_date = entries_array.inject { |memo, el| memo.merge(el) { |_k, old_v, new_v| old_v + new_v } }
+    entries_array.inject { |memo, el| memo.merge(el) { |_k, old_v, new_v| old_v + new_v } }
   end
 
-  # Gets the count of entries by category 
+  # Gets the count of entries by category
   def fetch_activity_by_category(type)
     public_send(type).joins(:category).group(:name).count(:name)
   end
 
-  # Gets a hash containing the total of all activity by category 
+  # Gets a hash containing the total of all activity by category
   def fetch_activities_by_category(*types)
     entries_array = []
-    types.each_with_object({}) do | key, hash |
+    types.each_with_object({}) do |key, hash|
       entries_array.push(hash[key] = fetch_activity_by_category(key))
     end
-    total_entries_by_category = entries_array.inject { |memo, el| memo.merge(el) { |_k, old_v, new_v| old_v + new_v } }
+    entries_array.inject { |memo, el| memo.merge(el) { |_k, old_v, new_v| old_v + new_v } }
   end
 
-    # Gets the count of entries by language 
-    def fetch_activity_by_language(type)
-      public_send(type).joins(:language).group(:name).count(:name)
-    end
-  
-    # Gets a hash containing the total of all activity by category 
-    def fetch_activities_by_language(*types)
-      entries_array = []
-      types.each_with_object({}) do | key, hash |
-        entries_array.push(hash[key] = fetch_activity_by_language(key))
-      end
-      total_entries_by_language = entries_array.inject { |memo, el| memo.merge(el) { |_k, old_v, new_v| old_v + new_v } }
-    end
+  # Gets the count of entries by language
+  def fetch_activity_by_language(type)
+    public_send(type).joins(:language).group(:name).count(:name)
+  end
 
-    def fetch_daily_counts(type)
-      public_send(type).where(created_at:
-        (Time.zone.now.midnight - 1.day)..Time.zone.now.midnight).count
+  # Gets a hash containing the total of all activity by category
+  def fetch_activities_by_language(*types)
+    entries_array = []
+    types.each_with_object({}) do |key, hash|
+      entries_array.push(hash[key] = fetch_activity_by_language(key))
     end
+    entries_array.inject { |memo, el| memo.merge(el) { |_k, old_v, new_v| old_v + new_v } }
+  end
 
-    def fetch_weekly_counts(type)
-      public_send(type).where(created_at:
-        (Time.zone.now.midnight - 7.days)..Time.zone.now.midnight).count
-    end
+  def fetch_daily_counts(type)
+    public_send(type).where(created_at:
+      (Time.zone.now.midnight - 1.day)..Time.zone.now.midnight).count
+  end
+
+  def fetch_weekly_counts(type)
+    public_send(type).where(created_at:
+      (Time.zone.now.midnight - 7.days)..Time.zone.now.midnight).count
+  end
 end
-
-
-
-
-
-
-
-
-
-
 
 # returns a hash however I want an array with a hash inside
 # alternative merge method though
